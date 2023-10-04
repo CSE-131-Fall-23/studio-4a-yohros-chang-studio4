439e5672bad7341c93193748048f3c6bd9006d84
zgira <zgira@10.232.174.30>
2023-10-04 15:42:33 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,25 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		// Rectangles
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.25);
+		
+		StdDraw.setPenColor(200, 200, 255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.395, 0.245);
+		
+		// Fish body
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.setPenRadius(0.005);
+		StdDraw.ellipse(0.55, 0.5, 0.12, 0.07);
+		
+		// Fish tail
+		double[] x = {0.428, 0.35, 0.35};
+		double[] y = {0.5, 0.55, 0.45};
+		StdDraw.polygon(x, y);
+		
+		// Text
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.text(0.5, 0.375, "be nice, eat fish");
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,48 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double xRadius = in.nextDouble();
+		double yRadius = in.nextDouble();
+		
+		StdDraw.setPenColor(r, g, b);
+		
+		if (shape.equals("rectangle"))
+		{
+			if (isFilled == true)
+				StdDraw.filledRectangle(x, y, xRadius, yRadius);
+			else
+				StdDraw.rectangle(x, y, xRadius, yRadius);
+		}
+		
+		if (shape.equals("ellipse"))
+		{
+			if (isFilled == true)
+				StdDraw.filledEllipse(x, y, xRadius, yRadius);
+			else
+				StdDraw.ellipse(x, y, xRadius, yRadius);
+		}
+		
+		if (shape.equals("triangle"))
+		{
+			double a = in.nextDouble();
+			double z = in.nextDouble();
+			
+			double[] xs = {x, xRadius, a};
+			double[] ys = {y, yRadius, z};
+			
+			if (isFilled == true)
+				StdDraw.filledPolygon(xs, ys);
+			else
+				StdDraw.polygon(xs, ys);
+		}
+
+		
 	}
 }
